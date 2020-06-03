ce121db7456df87e58fa7878d76e7cd34cc4514b
am10 <adventam10@gmail.com>
2020-06-04 08:36:44 +0900

    Add spinner event listner

--- WidgetSample/app/src/main/java/com/example/widgetsample/ContainerActivity.kt
+++ WidgetSample/app/src/main/java/com/example/widgetsample/ContainerActivity.kt
@@ -2,10 +2,16 @@ package com.example.widgetsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
 class ContainerActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = ContainerActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +23,19 @@ class ContainerActivity : AppCompatActivity() {
             android.R.layout.simple_spinner_item, spinnerItems)
 //        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            //　アイテムが選択された時
+            override fun onItemSelected(parent: AdapterView<*>?,
+                                        view: View?, position: Int, id: Long) {
+                val spinnerParent = parent as Spinner
+                val item = spinnerParent.selectedItem as String
+                Log.d(TAG, "onItemSelected! item: $item, position: $position, id: $id")
+            }
+
+            //　アイテムが選択されなかった
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Log.d(TAG, "onNothingSelected!")
+            }
+        }
     }
 }
